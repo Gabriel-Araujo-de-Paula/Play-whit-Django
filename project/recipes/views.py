3724b7d49c0dbcbc7336f853852444cf086e21cd
Gabriel Araújo de Paula <gabbypaula793@gmail.com>
2024-07-12 19:47:20 -0300

    ajustes adicionais

--- project/recipes/views.py
+++ project/recipes/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'global/home.html')
+    return render(request, 'recipes/home.html', context={
+        'name': 'gabby paula'
+    })
 
 
 def about(request):
