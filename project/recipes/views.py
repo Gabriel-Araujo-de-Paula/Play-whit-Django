de2d5ee441153d674cdff29faa233d8b8e0ab081
Gabriel Araújo de Paula <gabbypaula793@gmail.com>
2024-07-14 23:40:24 -0300

    configurando urls dinâmicas

--- project/recipes/views.py
+++ project/recipes/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'recipes/pages/home.html')
 
-def recipe(request):
+def recipe(request,id):
     return render(request, 'recipes/pages/home.html')
 
 
